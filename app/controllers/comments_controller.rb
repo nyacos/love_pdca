08f61181f182f20f5a54f989343fec2ee429a3c1
nyacos <m_kutipatti23@yahoo.co.jp>
2023-12-13 14:58:15 +0900

    FIX: ボタンの記載修正

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -8,9 +8,9 @@ class CommentsController < ApplicationController
   def create
     comment = current_user.comments.build(comment_params)
     if comment.save
-      redirect_to act_path(comment.act), success: t('defaults.message.created', item: Comment.model_name.human)
+      redirect_to act_path(comment.act), success: 'Comment was successfully created'
     else
-      redirect_to act_path(comment.act), danger: t('defaults.message.not_created', item: Comment.model_name.human)
+      redirect_to act_path(comment.act), danger: 'Failed to create comment.'
     end
   end
 
